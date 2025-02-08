6758cd99cf8eb8c3b3f14b501a22f8247f828520
Dominic Chen <dominic.chen630@gmail.com>
2025-02-07 22:28:27 -0500

    tailwind configf

--- client/tailwind.config.mjs
+++ client/tailwind.config.mjs
@@ -1,3 +1,5 @@
+import { Jaro, Monomaniac_One } from 'next/font/google';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,9 +10,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#000416",
+        textGray: "#8A8A8A",
       },
+      fontFamily: {
+        Monomaniac: ["Monomaniac One", 'serif'],
+        Jaro: ["Jaro", 'serif']
+      }
     },
   },
   plugins: [],

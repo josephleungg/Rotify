74bfc896e8c40fd43a0099a88fc6d7b876b333f0
Tristan Cheng <cheng.tristan@hotmail.com>
2025-02-08 00:39:00 -0500

    fixed video sizing

--- client/components/VideoModule.jsx
+++ client/components/VideoModule.jsx
@@ -3,9 +3,9 @@
 import React, { useRef, useEffect } from 'react';
 
 const videoFiles = [
-    "./videos/minecraft.mp4",
-    "./videos/gta5.mp4"
-]
+  "./videos/minecraft.mp4",
+  "./videos/gta5.mp4"
+];
 
 const getRandomVideo = () => {
   const randomIndex = Math.floor(Math.random() * videoFiles.length);
@@ -32,7 +32,9 @@ const VideoModule = () => {
       const endTime = startTime + 30;
       const checkTime = () => {
         if (videoElement.currentTime >= endTime) {
-          videoElement.pause();
+          // Reset to startTime and play again
+          videoElement.currentTime = startTime;
+          videoElement.play();
         } else {
           requestAnimationFrame(checkTime);
         }
@@ -50,20 +52,23 @@ const VideoModule = () => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center relative w-screen h-screen">
+      <div style={{ width: '30%', height: 'auto', maxWidth: '100%', maxHeight: '100%' }}>
         <video
           ref={videoRef}
           style={{
             width: '100%',
             height: '100%',
-            objectFit: 'cover',
-            objectPosition: 'center top',
+            objectFit: 'contain', // Use 'contain' to ensure the entire video is visible
           }}
           muted
           controls={false}
+          loop // Add the loop attribute for seamless looping
+          disablePictureInPicture
         >
           Your browser does not support the video tag.
         </video>
+      </div>
       <div>
         <button onClick={handlePlay}>Play</button>
         <button onClick={handlePause}>Pause</button>

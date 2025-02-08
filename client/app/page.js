2c596d87769e938acc0cff21c2a00acdebc32420
Tristan Cheng <cheng.tristan@hotmail.com>
2025-02-07 22:43:38 -0500

    home page layout

--- client/app/page.js
+++ client/app/page.js
@@ -1,9 +1,42 @@
+'use client'
+import react from "react";
+import { useState } from "react";
+
 import Image from "next/image";
 
 export default function Home() {
+
+  // const [urlDisplay]
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      test
+    <div className="flex flex-col gap-32 items-center justify-center h-screen bg-slate-950">
+      <div className="flex flex-col gap-4 items-center justify-center">
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          width={72}
+          height={16}
+        />
+        <h1 className="text-4xl">Rotify</h1>
+        <p>
+          Turn informaton into an engaging, brain-boosting video!
+        </p>
+      </div>
+      <div className="flex flex-col gap-4 items-center justify-center">
+        <p>
+          Drop a link to any website or article, and we'll transform it into an immersive, interactive video
+        </p>
+        <input
+          type="text"
+          placeholder="Enter URL"
+          className="border-2 border-gray-200 p-2 rounded-lg w-[45vw]">
+        </input>
+      </div>
+      <div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Generate
+        </button>
+      </div>
+       
     </div>
   );
 }

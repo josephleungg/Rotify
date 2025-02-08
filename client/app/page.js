bc59e023f1244117fcd9f3b03f745510acfe25d3
Tristan Cheng <cheng.tristan@hotmail.com>
2025-02-07 23:58:21 -0500

    component state manager

--- client/app/page.js
+++ client/app/page.js
@@ -1,41 +1,55 @@
 'use client'
-import react from "react";
 import { useState } from "react";
+import UrlField from "@/components/UrlField";
+import FileUpload from "@/components/fileUpload";
+import Conversions from "@/components/conversions";
 
 import Image from "next/image";
 
 export default function Home() {
 
-  // const [urlDisplay]
+  const [conversionsDisplay, setConversionsDisplay] = useState(true);
+  const [urlDisplay, setUrlDisplay] = useState(false);
+  const [fileUploadDisplay, setFileUploadDisplay] = useState(false);
+
+  const fileUploadButton = () => {
+    setConversionsDisplay(false);
+    setUrlDisplay(false);
+    setFileUploadDisplay(true);
+  }
+
+  const urlButton = () => {
+    setConversionsDisplay(false);
+    setUrlDisplay(true);
+    setFileUploadDisplay(false);
+  }
+
+  const backButton = () => {
+    setConversionsDisplay(true);
+    setUrlDisplay(false);
+    setFileUploadDisplay(false);
+  }
+
   return (
-    <div className="flex flex-col gap-32 items-center justify-center h-screen bg-slate-950">
-      <div className="flex flex-col gap-4 items-center justify-center">
+    <div className="flex flex-col gap-24 items-center justify-center h-screen bg-slate-950">
+
+      <div className="flex flex-col gap-1 items-center justify-center">
         <Image
-          src="/vercel.svg"
-          alt="Vercel Logo"
-          width={72}
-          height={16}
+          src="/rotify-logo.png"
+          alt="Rotify Logo"
+          width={144}
+          height={32}
+          crossOrigin="anonymous"
         />
-        <h1 className="text-4xl">Rotify</h1>
-        <p>
+        <h1 className="text-[48px] font-jaro">Rotify</h1>
+        <p className="font-jaro text-base text-[#8A8A8A]">
           Turn informaton into an engaging, brain-boosting video!
         </p>
       </div>
-      <div className="flex flex-col gap-4 items-center justify-center">
-        <p>
-          Drop a link to any website or article, and we'll transform it into an immersive, interactive video
-        </p>
-        <input
-          type="text"
-          placeholder="Enter URL"
-          className="border-2 border-gray-200 p-2 rounded-lg w-[45vw]">
-        </input>
-      </div>
-      <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Generate
-        </button>
-      </div>
+
+      {conversionsDisplay && <Conversions urlButton={urlButton} fileUploadButton={fileUploadButton} />}
+      {urlDisplay && <UrlField urlButton={urlButton} fileUploadButton={fileUploadButton} backButton={backButton} />}
+      {fileUploadDisplay && <FileUpload urlButton={urlButton} fileUploadButton={fileUploadButton} backButton={backButton} />}
        
     </div>
   );
--- client/components/fileUpload.jsx
+++ client/components/fileUpload.jsx
@@ -1,19 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCloudArrowUp } from "@fortawesome/free-solid-svg-icons"
 
-export default function FileUpload() {
+export default function FileUpload({ urlButton, backButton }) {
 
     return (
-        <div className="flex flex-col items-center justify-center">
-            <h1 className=" font-monomaniac text-textGray text-md py-4">
+        <div className="flex flex-col gap-4 items-center justify-center">
+            <h1 className=" font-monomaniac text-textGray text-md ">
                 Drop a file (ex. PDF, Slideshow, Article) we'll transform it into an immersive, interactive video
             </h1>
-            <div className="bg-[#414556] h-[150px] w-[150px] flex flex-col justify-center items-center rounded-xl cursor-pointer">
+            <div className="bg-[#414556] h-[150px] w-[150px] flex justify-center items-center rounded-xl cursor-pointer">
                     <FontAwesomeIcon icon={faCloudArrowUp} className="h-[80px] w-auto text-background"/>
             </div>
-            <div className="bg-[#685680] h-[40px] w-[150px] flex flex-col justify-center items-center rounded-lg cursor-pointer mt-12">
-                <h1 className="text-[#B1A6C0] font-jaro text-2xl">Generate</h1>
+            <div className="flex flex-row gap-8">
+                <div className="bg-[#685680] h-[50px] flex px-8 justify-center items-center rounded-lg cursor-pointer hover:scale-95 transition-all">
+                    <h1 className="text-[#B1A6C0] font-jaro text-3xl">Generate</h1>
+                </div>
+                <div 
+                    className="bg-transparent border-[2px] border-[#B1A6C0] h-[50px] px-8 flex flex-col justify-center items-center rounded-lg cursor-pointer hover:scale-95 transition-all"
+                    onClick={backButton}
+                >
+                    <h1 className="text-[#685680] font-jaro text-3xl">Back</h1>
+                </div>
             </div>
+            
         </div>
     )
 }
